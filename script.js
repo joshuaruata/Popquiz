337829b6df471842a9c4e20426a7539186b0df23
joshuaruata <70530293+joshuaruata@users.noreply.github.com>
2020-09-21 11:08:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+
+var number = 5;
+var prompter = prompt("Enter a number");
+if(isNaN(prompter)){
+  repeat=prompt("Enter a number");
+}
+(prompt<=10)
+  wrapperEle.innerHTML="Program is over";
